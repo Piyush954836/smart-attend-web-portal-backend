f18ff1e5b4b86bfa830ce73a3907070d6763f1ee
Piyush954836 <panwarpiyush653@gmail.com>
2026-03-03 21:54:59 +0530

    Added new URL in CORS

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import studentRoutes from './routes/studentRoutes.js';
 
 const app = express();
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://your-production-app.vercel.app'],
+  origin: ['http://localhost:5173', 'https://6q9c843r-5173.inc1.devtunnels.ms', 'https://your-production-app.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
